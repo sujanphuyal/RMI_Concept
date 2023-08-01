92ef7195efe96a3191554cac40235b978fc68171
sujphu <sujphu@gmail.com>
2023-08-01 13:31:15 +0930

    Minimizing the codes

--- src/client.java
+++ src/client.java
@@ -1,19 +1,10 @@
 import java.rmi.Naming;
 
-public class client
-{
-    public static void main(String args[])
-    {
-        try
-        {
+public class client {
+    public static void main(String[] args) throws Exception {
             String ip="rmi://localhost/RemoteInterface";
             RemoteInterface s=
                     (RemoteInterface)Naming.lookup(ip);
             System.out.println("sum: "+ s.add(1,3));
-        }
-        catch(Exception e)
-        {
-            System.out.println(e.getMessage());
-        }
     }
 }
--- src/server.java
+++ src/server.java
@@ -2,14 +2,10 @@ import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 
 public class server {
-    public static void main(String[] args) {
-        try{
+    public static void main(String[] args) throws Exception{
             ServerImplement s=new ServerImplement();
             LocateRegistry.createRegistry(1099);
             Naming.rebind("rmi:///RemoteInterface",s);
             System.out.println("Server Started ");
-        }catch (Exception e){
-            System.out.println(e.getMessage());
-        }
     }
 }
