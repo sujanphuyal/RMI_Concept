59859267e460d49feba788aa33ad2285d6b3b210
sujphu <sujphu@gmail.com>
2023-08-01 17:07:20 +0930

    Normalizing code to run via cmd

--- src/client.java
+++ src/client.java
@@ -1,10 +1,7 @@
 import java.rmi.Naming;
-
 public class client {
     public static void main(String[] args) throws Exception {
-            String ip="rmi://localhost/RemoteInterface";
-            RemoteInterface s=
-                    (RemoteInterface)Naming.lookup(ip);
-            System.out.println("sum: "+ s.add(1,3));
+        RemoteInterface s= (RemoteInterface)Naming.lookup("RemoteInterface");
+        System.out.println("sum: "+ s.add(1,3));
     }
 }
--- src/server.java
+++ src/server.java
@@ -1,11 +1,8 @@
 import java.rmi.Naming;
-import java.rmi.registry.LocateRegistry;
-
 public class server {
     public static void main(String[] args) throws Exception{
-            ServerImplement s=new ServerImplement();
-            LocateRegistry.createRegistry(1099);
-            Naming.rebind("rmi:///RemoteInterface",s);
-            System.out.println("Server Started ");
+        ServerImplement s=new ServerImplement();
+        Naming.rebind("RemoteInterface",s);
+        System.out.println("Server Started ");
     }
 }
